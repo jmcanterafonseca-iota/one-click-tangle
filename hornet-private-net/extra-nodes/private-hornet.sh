cc88f892804fb5ffa01f0d43452f2de74f86dd00
Jose Manuel Cantera <jose.cantera@iota.org>
2021-08-02 13:08:35 +0200

    Next snapshot

--- hornet-private-net/extra-nodes/private-hornet.sh
+++ hornet-private-net/extra-nodes/private-hornet.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Script to add a new Hornet Node to a Private Tangle
-# private-hornet.sh [install|start|stop] <node_name> <coo_public_key>? <peer_address>?
+# private-hornet.sh [install|start|stop] <node_details> <coo_public_key>? <peer_address>?
+# node_details must be a colon separated string including "node_name:api_port:peering_port:dashboard_port"
+# example "mynode:14627:15601:8082"
 
 set -e
 
@@ -9,7 +11,7 @@ set -e
 source ../utils.sh
 
 help () {
-  echo "usage: private-hornet.sh [install|start|stop] <node_name> <coo_public_key>? <peer_address>?"
+  echo "usage: private-hornet.sh [install|start|stop] <node_details> <coo_public_key>? <peer_address>?"
 }
 
 if [ $#  -lt 2 ]; then
@@ -19,7 +21,31 @@ if [ $#  -lt 2 ]; then
 fi
 
 command="$1"
-node_name="$2"
+node_details="$2"
+
+IFS=':'
+read -a node_params <<< "$node_details"
+
+DEFAULT_API_PORT=14265
+DEFAULT_PEERING_PORT=15600
+DEFAULT_DASHBOARD_PORT=8081
+
+node_name=${node_params[0]}
+api_port=${node_params[1]}
+peering_port=${node_params[2]}
+dashboard_port=${node_params[3]}
+
+if [ -n "$api_port" ]; then
+  api_port=$DEFAULT_API_PORT
+fi
+
+if [ -n "$peering_port" ]; then
+  peering_port=$DEFAULT_PEERING_PORT
+fi
+
+if [ -n "$dashboard_port" ]; then
+  dashboard_port=$DEFAULT_DASHBOARD_PORT
+fi
 
 if [ -n "$3" ]; then
   coo_public_key="$3"
@@ -60,8 +86,8 @@ clean () {
     sudo rm -Rf ./p2pstore
   fi
 
-  if [ -f ./config/peering.json ]; then
-    sudo rm -f ./config/peering.json
+  if [ -d ./config ]; then
+    sudo rm -Rf ./config
   fi
 }
 
@@ -102,8 +128,17 @@ volumeSetup () {
 bootstrapFiles () {
   cp ../../docker-compose.yml .
   sed -i 's/node/'$node_name'/g' docker-compose.yml
+  sed -i 's/'$DEFAULT_API_PORT'/'$api_port'/g' docker-compose.yml
+  sed -i 's/'$DEFAULT_PEERING_PORT'/'$peering_port'/g' docker-compose.yml
+  sed -i 's/'$DEFAULT_DASHBAOARD_PORT'/'$dashboard_port'/g' docker-compose.yml
+
   cp ../../../config/config-node.json ./config/config.json
+  sed -i 's/'$DEFAULT_API_PORT'/'$api_port'/g' ./config/config.json
+  sed -i 's/'$DEFAULT_PEERING_PORT'/'$peering_port'/g' ./config/config.json
+  sed -i 's/'$DEFAULT_DASHBAOARD_PORT'/'$dashboard_port'/g' ./config/config.json
+
   cp ../../../config/profiles.json ./config/profiles.json
+  cp ../../peering.json ./config/peering.json
 }
 
 installNode () {
